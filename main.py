1e63bd65f29ad9341adae5cc603557eb2cde9a33
Niklas <138971666+keimschleuder@users.noreply.github.com>
2025-07-08 16:10:27 +0200

    Sone prepwork

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import argparse
+import gpiod
 
+# Console Arguments init
 parser = argparse.ArgumentParser(prog = "main.py", 
                                  usage = "python main.py [-h] [--headless]", 
                                  description = "Intended to run on a Raspberry Pi 5 with AI-HAT and AI-Camera. It detects harmful plants and destroys them.", 
                                  epilog = "Program written for the 'Jugend Forscht'-Competition by Niklas Keim", 
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('--headless', action='store_true')
-
-args = parser.parse_args()
-headless = args.headless
+# GPIO init
+chip = gpiod.Chip('gpiochip4')
